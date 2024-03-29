6a061e88ca5362538c4ae59ac100313ce5a49fc5
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-03-29 11:10:44 +0700

    masukkan tipe data

--- Quick sort/Quick sort.cpp
+++ Quick sort/Quick sort.cpp
@@ -99,6 +99,15 @@ void display() {
 	cout << "number of data movement: " << mov_count << endl;
 }
 
-int main() {
+int main()
+{
+	char ch;
 
+	do {
+		input();
+		q_short(0, n - 1);
+		display();
+		cout << "\n\n do you want to continue? (y/n):";
+		cin >> ch;
+	}
 }
