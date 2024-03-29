b570c6bf2525944873adc5cf9666c2c0d485c90c
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-03-29 09:35:36 +0700

    Add project files.

---
+++ Quick sort/Quick sort.cpp
@@ -0,0 +1,8 @@
+// Quick sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// array of integers to hold values
+int arr[20];
